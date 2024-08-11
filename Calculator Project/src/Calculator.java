a25d6dd497047e3802d5335b9afd3f32ea62aaaa
Vipashana <137157110+Vipashana@users.noreply.github.com>
2024-08-11 11:42:51 +0530

    Calculator.java

--- Calculator Project/src/Calculator.java
+++ Calculator Project/src/Calculator.java
@@ -9,8 +9,8 @@ class Calculator{
         return x-y;
     }
     //Method for Division
-    public static int div(int x, int y){
-        return x/y;
+    public static double div(int x, int y){
+        return (double)x/y;
     }
     //Method for Multiplication
     public static int mul(int x,int y){
@@ -18,28 +18,43 @@ class Calculator{
     }
 
     public static void main (String[] a){
-        //Creating scanner object to get user inputs
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1st number: ");
-        int num1 = sc.nextInt();
+        char choice;
+        do{
+            //Taking user inputs for numbers
+            System.out.println("Enter 1st number: ");
+            int num1 = sc.nextInt();
 
-        System.out.println("Enter 2nd number: ");
-        int num2 = sc.nextInt();
+            System.out.println("Enter 2nd number: ");
+            int num2 = sc.nextInt();
 
-        System.out.println("Enter operator: ");
-        String operator = sc.next();
-        //Operations based on user's operator
-        switch(operator){
-            case "+" -> System.out.println("Addition of "+num1+" and "+num2+ " is "+add(num1,num2));
+            //Taking user input for operator
+            System.out.println("Enter operator: ");
+            String operator = sc.next();
 
-            case "-" -> System.out.println("Subtraction of "+num1+" and "+num2+ " is "+sub(num1,num2));
+            //Operations based on user's operator
+            switch(operator) {
+                case "+" -> System.out.println("Addition of " + num1 + " and " + num2 + " is " + add(num1, num2));
 
-            case "/" -> System.out.println("Division of "+num1+" and "+num2+ " is "+div(num1,num2));
+                case "-" -> System.out.println("Subtraction of " + num1 + " and " + num2 + " is " + sub(num1, num2));
 
-            case "*" -> System.out.println("Multiplication of "+num1+" and "+num2+ " is "+mul(num1,num2));
+                case "/" -> System.out.println("Division of " + num1 + " and " + num2 + " is " + div(num1, num2));
 
-            default -> System.out.println("Invalid operator");
+                case "*" -> System.out.println("Multiplication of " + num1 + " and " + num2 + " is " + mul(num1, num2));
+
+                default -> System.out.println("Invalid operator");
+            }
+
+            // Ask user if they want to perform another calculation
+            System.out.println("Do you want to perform another operation? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        // Closing the scanner
+        sc.close();
+
+        System.out.println("Calculator exited.");
 
         }
     }
-}
